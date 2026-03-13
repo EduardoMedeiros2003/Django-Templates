e6de82774ff5b5e605ed0e3b08247432d8f2f371
Eduardo Umbelino <umbelino.eduardo2003@gmail.com>
2026-03-13 18:20:31 -0300

    feat: add static files structure and global stylesheet
    - Created static directory inside setup
    - Added assets folder for images and icons
    - Added styles folder for CSS files
    - Implemented global stylesheet (styles.css)
    - Organized project static files structure
    - Verified static configuration with collectstatic

--- setup/settings.py
+++ setup/settings.py
@@ -120,6 +120,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+#Informa o diretorio para que o django mexa com todos os arquivos estaticos
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'setup/static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
